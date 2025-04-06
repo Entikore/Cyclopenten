17ce9cbac9eb813240f24a6ab37aa3078c1e4057
entikore <m.grebe-lueth@proton.me>
2025-04-06 19:20:33 +0200

    build: switch to gradle version catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("com.android.application") version "8.9.1" apply false
-    id("com.android.library") version "8.9.1" apply false
-    id("com.google.dagger.hilt.android") version "2.53.1" apply false
-    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.google.dagger.hilt.android) apply false
+    alias(libs.plugins.google.devtools.ksp) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.jetbrains.kotlin.plugin.compose) apply false
 }
 
 tasks.register("clean", Delete::class){
